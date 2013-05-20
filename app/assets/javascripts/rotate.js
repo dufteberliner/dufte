ef6740fb5f226c572606ff354c7fb824c994bc09
Danny Weihs <danny.weihs@live.de>
2013-05-20 16:40:05 +0200

    Test COmmit

---
+++ app/assets/javascripts/rotate.js
@@ -0,0 +1,14 @@
+$(document).ready(function($) {
+				
+		$('#jq_fmslideshow').fmslideshow({
+						
+			banner_width : 770,
+			banner_height : 262,
+
+			button_nextPrevious_type : 0,
+			slideShow_loop : true,
+			buttons_spacing: '2,310'
+			
+			
+		});
+	});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dev001::Application.config.secret_token = 'fa8cbe1d03d325c673be0d23fd9e0e9f8ad58729ad606f8d1311e0d83ae32fbad880266c2ac765c2304ac3b33663275f7aaf3ad864810a08f7e32f039b0f31d1'
