894f2a370e1060632a9a96e29bd3e3c7b79ecef4
Danny Weihs <danny.weihs@live.de>
2013-05-22 17:49:43 +0200

    new commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require jquery-1.6.3.min
+//= require jquery-ui.min
+//= require cloud-zoom.1.0.2.min
+//= require cloud-zoom.1.0.2
+//= require skinable_tabs.min
